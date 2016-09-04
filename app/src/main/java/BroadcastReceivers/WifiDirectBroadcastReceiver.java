f72ffd950df014f70e7b8653946bc53486e78e4a
nikhilxebia <nsolanki@xebia.com>
2016-09-04 21:36:15 +0530

    @Updated the code

--- app/src/main/java/BroadcastReceivers/WifiDirectBroadcastReceiver.java
+++ app/src/main/java/BroadcastReceivers/WifiDirectBroadcastReceiver.java
@@ -33,8 +33,6 @@ public class WifiDirectBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
-
         String action = intent.getAction();
 
         if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
